ecd7523ea3ff1abf8e96ce809411905346a989e2
健悦 佐々木 <160196@jc-21.jp>
2018-11-26 12:47:08 +0900

    initial

---
+++ Bmi/Bmi/Bmi2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bmi
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Bmi2 : ContentPage
+	{
+        private float bmi;
+		public Bmi2 ()
+		{
+			InitializeComponent ();
+            buttonBack.Clicked += buttonBackClicked;
+		}
+
+        public Bmi2(float bmi)
+        {
+            InitializeComponent();
+            this.bmi = bmi;
+            labelBMI.Text = "BMI=" + bmi;
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
--- Bmi/Bmi/MainPage.xaml.cs
+++ Bmi/Bmi/MainPage.xaml.cs
@@ -9,9 +9,45 @@ namespace Bmi
 {
     public partial class MainPage : ContentPage
     {
+        float bmi;
+
         public MainPage()
         {
             InitializeComponent();
+            ButtonBMI.Clicked += ButtonBmiClickEvent;
+            ButtonBMI2.Clicked += ButtonBmi2ClickEvent;
+        }
+
+        private void ButtonBmi2ClickEvent(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            if (height <= 3.0)
+            {
+                height = height * height;
+            }
+            else
+            {
+                height = (height / 100) * (height / 100);
+            }
+            float weight = int.Parse(EntryWeight.Text);
+            bmi = weight / height;
+            Navigation.PushModalAsync(new NavigationPage(new Bmi2(bmi)));
+        }
+
+        private void ButtonBmiClickEvent(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            if (height <= 3.0)
+            {
+                height = height * height;
+            }
+            else
+            {
+                height = (height / 100) * (height / 100);
+            }
+            float weight = int.Parse(EntryWeight.Text);
+            bmi = weight / height;
+            LabelResult.Text = "BMI="+ bmi;
         }
     }
 }
